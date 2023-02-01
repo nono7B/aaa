ecfb08a8a492b0924f81ad652e61eb8eae824cbc
nono7B <113839355+nono7B@users.noreply.github.com>
2023-02-01 17:34:19 +0100

    Delete src directory

--- src/init-oauth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$discord_url = "https://discord.com/api/oauth2/authorize?client_id=1070360497309892730&redirect_uri=http%3A%2F%2Flocalhost%2Fyt-discord%2Fsrc%2Fprocess-oauth.php&response_type=code&scope=identify%20guilds";
-header("Location: $discord_url");
-exit();
-
-?>
